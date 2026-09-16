8c10ea02162589ce4d4a1cdb2a676d8182513931
sindhujasankaramoorthy <sindhuja.sankaramoorthy.work@gmail.com>
2026-09-16 20:21:51 +0530

    Add event and venue models

---
+++ Model/event.java
@@ -0,0 +1,22 @@
+public class Event {
+    private int eId;
+    private String eName;
+    private String type;
+    private String date;
+    private String startTime;
+    private String endTime;
+
+    private Venue venue;
+    private List<Organizer> organizers;
+
+    public Event(int eId, String eName, String type, String date, String startTime, String endTime, Venue venue, List<Organizer> organizers) {
+        this.eId = eId;
+        this.eName = eName;
+        this.type =  type;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.venue = venue;
+        this.organizers = organizers;
+    }
+}
---
+++ Model/venue.java
@@ -0,0 +1,13 @@
+public class Venue {
+    private int vId;
+    private String vName;
+    private String block;
+    private int capacity;
+
+    public Venue(int vId, String vName, String block, int capacity) {
+        this.vId = vId;
+        this.vName = vName;
+        this.block = block;
+        this.capacity = capacity;
+    }
+}
